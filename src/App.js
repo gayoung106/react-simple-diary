bf2a4794a2e642e992be06db913fca78e0b24fa8
송가영 <song-gayeong@song-gayeong-ui-MacBookAir.local>
2023-01-04 17:50:04 +0900

    list rendering & divide component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
+const dummyList = [
+  {
+    id: 1,
+    author: "author 1",
+    content: "content 1",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "author 2",
+    content: "content 2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "author 3",
+    content: "content 3",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "author 4",
+    content: "content 4",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
