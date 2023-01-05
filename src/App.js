cb43bff1156ebcc773ba56f595422a4fb39a7729
송가영 <song-gayeong@song-gayeong-ui-MacBookAir.local>
2023-01-05 14:25:56 +0900

    modifying array list

--- src/App.js
+++ src/App.js
@@ -38,6 +38,8 @@ function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  //DiaryEditor.js에서 매개변수를 받아옴 -> 이벤트의 형태일까?
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -51,15 +53,27 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
+
+  const onEdit = (targetId, newContent) => {
+    //매개변수로 받은건, 수정할 대상 그리고 어떤내용을 변경시킬것인지
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+    //map으로 돌려서 it(data)에 id가 수정하려는 id(타겟id)와 일치한다면,
+    //it객체를 전부 받아오고, 그 중 content객체의 내용은 newContent(변경시킬 내용)이 되고,
+    //일치하지 않는다면, it(원래 객체에 저장된 데이터)를 반환
+  };
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList onDelete={onDelete} diaryList={data} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const DiaryItem = ({
-  onDelete,
+  onEdit,
+  onRemove,
   author,
   content,
   emotion,
   created_date,
   id,
 }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  // isEdit이면 수정을 하지 않는 현재상태 = false
+  // setIsEdit은 수정을 하는 상태가 아니라 업데이트 되는 상태(즉, 업데이트 되지 않으면 현재상태가 되고, 업데이트 되면 업데이트 되는 내용이 현재상태가 됨)
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+  // 토글을 하면, 수정을 하는 상태(!isEdit)로 변화해서 그게 업데이트 되는 현재상태인 setIsEdit이 !isEdit의 상태로 업데이트해서 가지고 있음
+  // 그러니까 onClick을 할때마다 토글을 하면 이에 따라 변화한 상태(isEdit -> !isEdit / !isEdit->isEdit)가 setIsEdit이 됨
+
+  const localContentInput = useRef();
+  const handleRemove = () => {
+    if (window.confirm(`${id} 번째 일기를 삭제 하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+  //return이하에 함수를 넣게되면 코드가 지저분하고 길어지기 때문에 위에 handleRemove함수를 따로 만들어서 빼줌
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+    if (window.confirm(`${id}번째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
+    }
+  };
+  const [localContent, setLocalContent] = useState(content);
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -17,16 +49,30 @@ const DiaryItem = ({
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id} 번째 일기를 삭제 하시겠습니까?`)) {
-            onDelete(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            ></textarea>
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>취소</button>
+          <button onClick={handleEdit}>저장</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
