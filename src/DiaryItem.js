79e698b156b37de0134dd7285906bf6ae91fe9bb
송가영 <song-gayeong@song-gayeong-ui-MacBookAir.local>
2023-01-05 10:52:33 +0900

    deleted list array

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-const DiaryItem = ({ author, content, emotion, created_date, id }) => {
+const DiaryItem = ({
+  onDelete,
+  author,
+  content,
+  emotion,
+  created_date,
+  id,
+}) => {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -11,6 +18,15 @@ const DiaryItem = ({ author, content, emotion, created_date, id }) => {
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          if (window.confirm(`${id} 번째 일기를 삭제 하시겠습니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
